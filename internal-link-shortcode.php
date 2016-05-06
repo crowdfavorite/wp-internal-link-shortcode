9e2321780b96eadda659c45b88b63bedffec3211
suvajitpalcn <suvajitp.cn@gmail.com>
2016-05-06 10:48:16 +0530

    Updating Enqueue's to support wordpress updated process.

--- internal-link-shortcode.php
+++ internal-link-shortcode.php
@@ -102,12 +102,173 @@ function cfplsc_id_lookup() {
 }
 
 function cfplsc_admin_enqueue() {
-	wp_enqueue_script( 'cfplsc_admin_js', plugin_dir_url( __FILE__ ) . '/script.js', array('jquery') );
-	wp_localize_script( 'cfplsc_admin_js', 'Admin', array( 'index_url' => admin_url('index.php') ) );
+	wp_enqueue_script( 'cfplsc_admin_js', trailingslashit(get_bloginfo('url')).'?cf_action=cfplsc_admin_js', array('jquery') );
 
-	wp_enqueue_style( 'cfplsc_admin_css', plugin_dir_url( __FILE__ ) . '/style.css' );
+	wp_enqueue_style( 'cfplsc_admin_css', trailingslashit(get_bloginfo('url')).'?cf_action=cfplsc_admin_css' );
 }
 add_action( 'admin_enqueue_scripts', 'cfplsc_admin_enqueue' );
+
+function cfplsc_admin_js() {
+	header('Content-type: text/javascript');
+?>
+cfplsc_show_shortcode = function($elem) {
+	if ($elem.find('input').size() == 0) {
+		$elem.append('<input type="text" value="' + $elem.attr('title') + '" />').find('input').keydown(function(e) {
+			switch (e.which) {
+				case 13: // enter
+					return false;
+					break;
+				case 27: // esc
+					jQuery('#cfplsc_post_title').focus();
+					break;
+			}
+		}).focus().select();
+	}
+};
+jQuery(function($) {
+	$('#cfplsc_meta_box a.cfplsc_help').click(function() {
+		$('#cfplsc_meta_box div.cfplsc_readme').slideToggle(function() {
+			$('#cfplsc_post_title').css('background', '#fff');
+		});
+		return false;
+	});
+	$('#cfplsc_search_box').click(function() {
+		$('#cfplsc_post_title').focus().css('background', '#ffc');
+		return false;
+	});
+	$('#cfplsc_post_title').keyup(function(e) {
+		form = $('#cfplsc_meta_box');
+		term = $(this).val();
+// catch everything except up/down arrow
+		switch (e.which) {
+			case 27: // esc
+				form.find('.live_search_results ul').remove();
+				break;
+			case 13: // enter
+			case 38: // up
+			case 40: // down
+				break;
+			default:
+				if (term == '') {
+					form.find('.live_search_results ul').remove();
+				}
+				if (term.length > 2) {
+					$.get(
+						'<?php echo admin_url('index.php'); ?>',
+						{
+							cf_action: 'cfplsc_id_lookup',
+							post_title: term
+						},
+						function(response) {
+							$('#cfplsc_meta_box div.live_search_results').html(response).find('li').click(function() {
+								$('#cfplsc_meta_box .active').removeClass('active');
+								$(this).addClass('active');
+								cfplsc_show_shortcode($(this));
+								return false;
+							});
+						},
+						'html'
+					);
+				}
+				break;
+		}
+	}).keydown(function(e) {
+// catch arrow up/down here
+		form = $('#cfplsc_meta_box');
+		if (form.find('.live_search_results ul li').size()) {
+			switch (e.which) {
+				case 13: // enter
+					active = form.find('.live_search_results ul li.active');
+					if (active.size()) {
+						cfplsc_show_shortcode(active);
+					}
+					return false;
+					break;
+				case 40: // down
+					if (!form.find('.live_search_results ul li.active').size()) {
+						form.find('.live_search_results ul li:first-child').addClass('active');
+					}
+					else {
+						form.find('.live_search_results ul li.active').next('li').addClass('active').prev('li').removeClass('active');
+					}
+					return false;
+					break;
+				case 38: // up
+					if (!form.find('.live_search_results ul li.active').size()) {
+						form.find('.live_search_results ul li:last-child').addClass('active');
+					}
+					else {
+						form.find('.live_search_results ul li.active').prev('li').addClass('active').next('li').removeClass('active');
+					}
+					return false;
+					break;
+			}
+		}
+	});
+});
+<?php
+	die();
+}
+
+function cfplsc_admin_css() {
+	header('Content-type: text/css');
+?>
+#cfplsc_meta_box fieldset a.cfplsc_help {
+	background: #f5f5f5;
+	border-radius: 6px;
+	-moz-border-radius: 6px;
+	-webkit-border-radius: 6px;
+	color: #666;
+	display: block;
+	font-size: 11px;
+	float: right;
+	padding: 4px 6px;
+	text-decoration: none;
+}
+#cfplsc_meta_box fieldset label {
+	display: none;
+}
+#cfplsc_meta_box fieldset input {
+	width: 235px;
+}
+#cfplsc_meta_box .live_search_results {
+	position: relative;
+	z-index: 500;
+}
+#cfplsc_meta_box .live_search_results ul {
+	background: #fff;
+	list-style: none;
+	margin: 0 0 0 1px;
+	padding: 0 2px 3px;
+	position: absolute;
+	width: 230px;
+}
+#cfplsc_meta_box .live_search_results ul li {
+	border: 1px solid #eee;
+	border-top: 0;
+	cursor: pointer;
+	line-height: 100%;
+	margin: 0;
+	overflow: hidden;
+	padding: 5px;
+}
+#cfplsc_meta_box .live_search_results ul li.active,
+#cfplsc_meta_box .live_search_results ul li:hover {
+	background: #e0edf5;
+	font-weight: bold;
+}
+#cfplsc_meta_box .live_search_results input {
+	width: 200px;
+}
+#cfplsc_meta_box div.cfplsc_readme {
+	display: none;
+}
+#cfplsc_meta_box div.cfplsc_readme li {
+	margin: 0 10px 10px;
+}
+<?php
+	die();
+}
 
 function cfplsc_meta_box() {
 ?>
